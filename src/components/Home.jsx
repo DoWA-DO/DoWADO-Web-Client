1db3b82d67986d8df4caa585843a971c5735682e
jjjguz <wendy5371ok@gmail.com>
2024-07-10 13:24:57 +0900

    홈페이지 디자인 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,43 @@
 import React, { useState, useEffect, useRef } from "react";
-// import Dots from "./HomeDot";
 import "../ui/Home.css";
 import { useNavigate } from "react-router-dom";
+import { BsChevronCompactDown } from "react-icons/bs";
 
 const Home = () => {
   const navigate = useNavigate();
   const DIVIDER_HEIGHT = 5;
   const outerDivRef = useRef();
   const [currentPage, setCurrentPage] = useState(1);
+  const [content, setContent] = useState(""); // typing animation을 위한 상태 추가
+
+  useEffect(() => {
+    const writeText = (text) => {
+      let lines = text.split("\n");
+      let currentLine = 0;
+      let currentText = "";
+
+      const interval = setInterval(() => {
+        if (currentLine < lines.length) {
+          currentText += lines[currentLine].charAt(0);
+          setContent(currentText);
+          lines[currentLine] = lines[currentLine].substring(1);
+          if (lines[currentLine].length === 0) {
+            currentLine++;
+            currentText += "\n"; // 줄바꿈 추가
+          }
+        } else {
+          clearInterval(interval);
+        }
+      }, 80);
+
+      return () => clearInterval(interval);
+    };
+
+    writeText(`
+도와도 설명
+상담 도우미
+선생님 돕다`);
+  }, []);
 
   const handleClick = () => {
     navigate("/login");
@@ -23,7 +53,6 @@ const Home = () => {
       if (deltaY > 0) {
         // 스크롤 내릴 때
         if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
           console.log("현재 1페이지, down");
           outerDivRef.current.scrollTo({
             top: pageHeight + DIVIDER_HEIGHT,
@@ -32,7 +61,6 @@ const Home = () => {
           });
           setCurrentPage(2);
         } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
           console.log("현재 2페이지, down");
           outerDivRef.current.scrollTo({
             top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
@@ -41,7 +69,6 @@ const Home = () => {
           });
           setCurrentPage(3);
         } else {
-          // 현재 3페이지
           console.log("현재 3페이지, down");
           outerDivRef.current.scrollTo({
             top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
@@ -52,7 +79,6 @@ const Home = () => {
       } else {
         // 스크롤 올릴 때
         if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
           console.log("현재 1페이지, up");
           outerDivRef.current.scrollTo({
             top: 0,
@@ -60,7 +86,6 @@ const Home = () => {
             behavior: "smooth",
           });
         } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
           console.log("현재 2페이지, up");
           outerDivRef.current.scrollTo({
             top: 0,
@@ -69,7 +94,6 @@ const Home = () => {
           });
           setCurrentPage(1);
         } else {
-          // 현재 3페이지
           console.log("현재 3페이지, up");
           outerDivRef.current.scrollTo({
             top: pageHeight + DIVIDER_HEIGHT,
@@ -80,24 +104,32 @@ const Home = () => {
         }
       }
     };
+
     const outerDivRefCurrent = outerDivRef.current;
     outerDivRefCurrent.addEventListener("wheel", wheelHandler);
     return () => {
       outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
     };
   }, []);
+
   return (
     <div ref={outerDivRef} className="outer">
-      {/* <Dots currentPage={currentPage} /> */}
       <div className="inner home-1">
-        <img
-          src={`${process.env.PUBLIC_URL}/DoWADo_logo.png`}
-          alt="Logo"
-          className="home-logo"
-        />
+        <div className="home-logo-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/DoWADo_logo.png`}
+            alt="Logo"
+            className="home-logo"
+          />
+        </div>
+        <div className="home-content-container">
+          <h2 className="home-title">DoWA:DO</h2>
+          <div className="home-content">{content}|</div>
+          <BsChevronCompactDown className="blinking-cursor" />
+        </div>
       </div>
       <div className="divider"></div>
-      <div className="inner home-2">내용</div>
+      <div className="inner home-2"></div>
       <div className="divider"></div>
       <div className="inner home-3">
         <button className="start-btn" onClick={handleClick}>
